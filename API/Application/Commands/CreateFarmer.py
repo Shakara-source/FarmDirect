274d805f510451517ac997307b46ee7799dc42c1
Alexander Guerrero <guerreroalexander2@yahoo.com>
2022-03-24 21:26:38 -0400

    Added UOW interfaces for aggregates

--- API/Application/Commands/CreateFarmer.py
+++ API/Application/Commands/CreateFarmer.py
@@ -6,21 +6,7 @@ from Seedwork.Application.CommandHandlers import CommandResult
 from Seedwork.Application.Decorators import command_handler
 
 
-class CreateFarmerCommand(Command):
-    """A command for creating new user"""
+class FarmerCommands:
 
-    phoneNumber: str
-    password: str
-
-
-@command_handler
-def CreateFarmer(
-    command: CreateFarmerCommand, repository: FarmerRepo
-) -> CommandResult:
-    listing = Listing(id=repository.next_id(), **command.dict())
-    try:
-        repository.insert(listing)
-    except Exception as e:
-        return CommandResult.failed(message="Failed to create listing", exception=e)
-
-    return CommandResult.ok(result=listing.id)
+    def __init__(self) -> None:
+        self._farmer_repository = 
