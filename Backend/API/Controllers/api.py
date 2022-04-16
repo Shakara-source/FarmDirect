fdb2b9567984d2e15f44273c887cd61c44da98b2
Alexander Guerrero <guerreroalexander2@yahoo.com>
2022-04-16 15:40:55 -0400

    Updated HTTPSRedirectMiddleware

--- Backend/API/Controllers/api.py
+++ Backend/API/Controllers/api.py
@@ -16,13 +16,13 @@ origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
+    HTTPSRedirectMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.add_middleware(HTTPSRedirectMiddleware)
 
 Base.metadata.create_all(bind=engine)
 
